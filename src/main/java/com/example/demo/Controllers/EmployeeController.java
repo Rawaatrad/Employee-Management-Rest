558a5bd3e7f10f44b7f8a6f0fb1085b9604456b3
rawaa <rawaatrad@yahoo.com>
2021-10-06 11:05:28 +0100

    add employee Rest Api

--- src/main/java/com/example/demo/Controllers/EmployeeController.java
+++ src/main/java/com/example/demo/Controllers/EmployeeController.java
@@ -4,14 +4,12 @@ import com.example.demo.Entities.Employee;
 import com.example.demo.Repositories.EmployeeRepository;
 import org.apache.catalina.LifecycleState;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/employese")
+@RequestMapping("api/v1/employees")
 public class EmployeeController {
 
     @Autowired
@@ -21,6 +19,11 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+    //create employee
+    @PostMapping
+    public  Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
     }
 
 }
