100f7ab02cbb21ae8045bfc62267ca1dad1163ba
rawaa <rawaatrad@yahoo.com>
2021-10-06 11:28:45 +0100

    Update Employee Rest Api

--- src/main/java/com/example/demo/Controllers/EmployeeController.java
+++ src/main/java/com/example/demo/Controllers/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/employees")
@@ -24,6 +25,23 @@ public class EmployeeController {
     @PostMapping
     public  Employee createEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+    //get employee by id
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable Long id){
+        return employeeRepository.findById(id).get();
+    }
+
+    //update employee
+    @PutMapping("/{id}")
+    public  Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee){
+        Employee employee1 = employeeRepository.findById(id).get();
+
+        employee1.setFirstName(employee.getFirstName());
+        employee1.setLastName(employee.getLastName());
+        return employeeRepository.save(employee1);
+
+
     }
 
 }
